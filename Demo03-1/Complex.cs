720f9fdfac36b5a385967923ff54b0d903165ad8
Sedrick <2894607195@qq.com>
2026-04-03 15:44:50 +0800

    学习复数类表示

---
+++ Demo03-1/Complex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo03_1
+{
+    internal class Complex
+    {
+        public double Real {  get; set; }
+        public double Imaginary { get; set; }
+        public Complex(double r, double i)
+        {
+            Real = r;
+            Imaginary = i;
+        }
+        public Complex()
+        {
+            Real = 0;
+            Imaginary = 0;
+        }
+
+        public static Complex operator +(Complex left, Complex right)
+        {
+            var result = new Complex();
+            result.Real = left.Real + right.Real;
+            result.Imaginary = left.Imaginary + right.Imaginary;
+            return result;
+        }
+        public static Complex operator -(Complex left, Complex right)
+        {
+            var result = new Complex();
+            result.Real = left.Real - right.Real;
+            result.Imaginary = left.Imaginary - right.Imaginary;
+            return result;
+        }
+        public override string ToString()
+        {
+            string result = string.Empty;
+            if (Imaginary >= 0)
+            {
+                result = string.Format("{0}+{1}i", Real, Imaginary);
+            }
+            else
+            {
+                result = string.Format("{0}-{1}i", Real, -Imaginary);
+            }
+                return result;
+        }
+
+    }
+}
---
+++ Demo03-1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo03_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new Complex(3, 4);
+            var y = new Complex(7, 8);
+            Console.WriteLine("x={0};y={1}",x.ToString() , y.ToString());
+            var z = x + y;
+            var a = x - y;
+            Console.WriteLine("z={0};a={1}",z.ToString() , a.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
