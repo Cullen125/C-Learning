37e4ca56e3b07271f99c087828aa2cf78f53e5e1
Sedrick <2894607195@qq.com>
2026-04-03 15:48:08 +0800

    学习继承、抽象类、接口、将类编译成为动态链接库

---
+++ Demo05/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pda;
+namespace Demo05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Phone p1=new Phone();
+            //p1.PhoneNumber = "12345678";
+            //Contract c1=new Contract();
+            //c1.Address = "129 Luoroad, Wuhan, Hubei, P.R.China";
+
+            Contract c1 = new Contract();
+            c1.FirstName = "Y.B.";
+            c1.FamilyName = "Li";
+            c1.Address = "129 Luoyu Road, Wuhan";
+
+
+            Console.WriteLine(c1.FirstName+" "+c1.FamilyName+" "+c1.Address);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pda/Phone.cs
@@ -0,0 +1,29 @@
+﻿namespace Pda
+{
+    public class Phone
+    {
+        public string PhoneNumber {  get; set; }
+    }
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+        string FamilyName { get; set; }
+    }
+    public class Contract : Phone,IPerson
+    {
+        public string Address { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+
+        public Contract()
+        {
+            PhoneNumber = "1234555";
+            Address = "ahhh";
+
+        }
+        public Contract(string phoneName)
+        {
+            PhoneNumber = phoneName;
+        }
+    }
+}
